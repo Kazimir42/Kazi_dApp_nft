6d400e66369928338d929c8becfbefbc131ecc35
kazi <champlonremii@gmail.com>
2022-04-23 10:42:26 +0200

    basic routing

---
+++ reactapp/src/pages/Whitelist.js
@@ -0,0 +1,20 @@
+
+
+function Whitelist() {
+    return (
+        <div className="text-4xl">
+            <img src="/logo.svg" className="h-32 w-32 mx-auto" alt="logo" />
+            <p className="text-4xl">
+                Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </div>
+    )
+}export default Whitelist;
