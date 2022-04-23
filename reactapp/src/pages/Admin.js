7d7801eae3a13c497727683624d9f9af156c30ff
kazi <champlonremii@gmail.com>
2022-04-23 20:42:13 +0200

    can paused contract

--- reactapp/src/pages/Admin.js
+++ reactapp/src/pages/Admin.js
@@ -3,7 +3,6 @@ import {BigNumber, ethers} from 'ethers';
 import Contract from '../artifacts/contracts/Character.sol/Character.json'
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
-const tokens = require("./tokens.json");
 
 const address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
@@ -15,10 +14,65 @@ function Admin() {
     const [priceSale, setPriceSale] = useState(0)
     const [merkleRoot, setMerkleRoot] = useState(0)
     const [tokensFile, setTokensFile] = useState(0)
+    const [paused, setPaused] = useState(false)
 
     useEffect(() => {
         getData();
     }, [])
+
+
+
+    async function getData() {
+        if (typeof window.ethereum !== 'undefined') {
+            let chainId = await window.ethereum.request({method: 'eth_chainId'})
+            if (chainId === "0x1" || chainId === "0x3" || chainId === "0x4" || chainId === "0x5" || chainId === "0x2a" || chainId === "0x539") {
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                //get the contract
+                const contract = new ethers.Contract(address, Contract.abi, provider);
+                try {
+                    //call functions to gets data in contract
+                    const totalNft = await contract.MAX_SUPPLY();
+                    const maxMint = await contract.max_mint_allowed();
+                    const step = await contract.sellingStep();
+                    const pricePresale = await contract.pricePresale();
+                    const priceSale = await contract.priceSale();
+                    const merkleRoot = await contract.merkleRoot();
+                    const paused = await contract.paused();
+
+                    setTotalNft(Number(totalNft))
+                    setMaxMint(Number(maxMint))
+                    setCurrentStep(step)
+                    setPricePresale(ethers.utils.formatEther(Number(pricePresale)))
+                    setPriceSale(ethers.utils.formatEther(Number(priceSale)))
+                    setMerkleRoot(merkleRoot)
+                    setPaused(paused)
+
+                }catch(error) {
+                    console.log(error)
+                }
+            }
+        }
+    }
+
+
+    async function pausedContract(boolean) {
+        if (typeof window.ethereum !== 'undefined') {
+            let chainId = await window.ethereum.request({method: 'eth_chainId'})
+            if (chainId === "0x1" || chainId === "0x3" || chainId === "0x4" || chainId === "0x5" || chainId === "0x2a" || chainId === "0x539") {
+                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                const signer = provider.getSigner();
+                //get the contract
+                const contract = new ethers.Contract(address, Contract.abi, signer);
+                try {
+                    //update contract
+                    contract.setPaused(boolean)
+                } catch (error) {
+                    console.log(error)
+                }
+            }
+        }
+    }
+
 
 
     function uploadFile(event) {
@@ -52,39 +106,6 @@ function Admin() {
                 try {
                     //update contract
                     contract.changeMerkleRoot(root)
-                }catch(error) {
-                    console.log(error)
-                }
-            }
-        }
-
-
-    }
-
-
-    async function getData() {
-        if (typeof window.ethereum !== 'undefined') {
-            let chainId = await window.ethereum.request({method: 'eth_chainId'})
-            if (chainId === "0x1" || chainId === "0x3" || chainId === "0x4" || chainId === "0x5" || chainId === "0x2a" || chainId === "0x539") {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
-                //get the contract
-                const contract = new ethers.Contract(address, Contract.abi, provider);
-                try {
-                    //call functions to gets data in contract
-                    const totalNft = await contract.MAX_SUPPLY();
-                    const maxMint = await contract.max_mint_allowed();
-                    const step = await contract.sellingStep();
-                    const pricePresale = await contract.pricePresale();
-                    const priceSale = await contract.priceSale();
-                    const merkleRoot = await contract.merkleRoot();
-
-                    setTotalNft(Number(totalNft))
-                    setMaxMint(Number(maxMint))
-                    setCurrentStep(step)
-                    setPricePresale(ethers.utils.formatEther(Number(pricePresale)))
-                    setPriceSale(ethers.utils.formatEther(Number(priceSale)))
-                    setMerkleRoot(merkleRoot)
-
                 }catch(error) {
                     console.log(error)
                 }
@@ -145,7 +166,8 @@ function Admin() {
 
             <div className="mt-10">
                 <h2 className="text-2xl font-bold underline">Infos</h2>
-                <p>Merkle root : {merkleRoot}</p>
+                <p>Contract paused : {paused?'true':'false'}</p>
+                <p>Whitelist Merkle root : {merkleRoot}</p>
                 <p>NTFs number : {totalNft}</p>
                 <p>Max mint : {maxMint}</p>
                 <p>Current step : {currentStep}</p>
@@ -156,7 +178,18 @@ function Admin() {
             <div className="mt-10">
                 <h2 className="text-2xl font-bold underline">Update contract</h2>
                 <div className="my-4">
-                    <p>Merkle root :</p>
+                    <p>Contract status:</p>
+
+                    {
+                        paused
+                            ?
+                            <button className="bg-blue-500 text-white px-2 hover:bg-blue-600 p-1 border border-blue-500" onClick={() => pausedContract(false)}>Unpause</button>
+                            :
+                            <button className="bg-blue-500 text-white px-2 hover:bg-blue-600 p-1 border border-blue-500" onClick={() => pausedContract(true)}>Pause</button>
+                    }
+                </div>
+                <div className="my-4">
+                    <p>Whitelist Merkle root :</p>
                     <input type="file" name="file" onChange={uploadFile} />
 
                     <button className="bg-blue-500 text-white px-2 hover:bg-blue-600 p-1 border border-blue-500" onClick={newMerkleRoot}>valider</button>
